44e3b1926eeb9825a150505f1f178a639377c4dc
Roberth Narvaez <roberth.narvaez@ceiba.com.co>
2022-05-26 09:22:09 -0500

    Eliminando numeros magicos

--- microservicio/dominio/src/main/java/com/ceiba/matricula_estudiante/modelo/entidad/MatriculaEstudiante.java
+++ microservicio/dominio/src/main/java/com/ceiba/matricula_estudiante/modelo/entidad/MatriculaEstudiante.java
@@ -16,7 +16,6 @@ public class MatriculaEstudiante {
     private static final String SE_DEBE_INGRESAR_DATOS_DEL_SALON_DEL_ESTUDIANTE = "Se debe ingresar los datos del salón del estudiante";
     private static final String SE_DEBE_INGRESAR_LA_JORNADA_DEL_ESTUDIANTE = "Se debe ingresar la jornada del estudiante";
     private static final String SE_DEBE_INGRESAR_LA_FECHA_DE_INGRESO_DEL_ESTUDIANTE = "Se debe ingresar la fecha de ingreso del estudiante";
-    private static final String SE_DEBE_INGRESAR_UN_IDENTIFICADOR_DE_SALA_CORRECTO = "Se debe ingresar un identificador de sala correcto";
     private static final String SE_DEBE_INGRESAR_UNA_JORNADA_CORRECTA = "Se debe ingresar una jornada correcta";
     public static final String LA_FECHA_NO_PUEDE_SER_IGUAL_AL_DIA_DE_DESCANSO = "La fecha no puede ser igual a un dia de descanso";
     public static final String LA_FECHA_NO_PUEDE_SER_MENOR_AL_DIA_ACTUAL = "La fecha no puede ser menor al dia de actual";
@@ -34,7 +33,6 @@ public class MatriculaEstudiante {
         validarObligatorio(idEstudiante, SE_DEBE_INGRESAR_DATOS_DEL_ESTUDIANTE);
         validarObligatorio(idDocenteSala, SE_DEBE_INGRESAR_DATOS_DEL_SALON_DEL_ESTUDIANTE);
 
-        validarIdentificadorSalaCorrecto(idDocenteSala, SE_DEBE_INGRESAR_UN_IDENTIFICADOR_DE_SALA_CORRECTO);
         validarJornadaCorrecto(jornada, SE_DEBE_INGRESAR_UNA_JORNADA_CORRECTA);
         validarFechaNoPuedeSerInferiorAlDiaDeHoy(fechaIngreso, LA_FECHA_NO_PUEDE_SER_MENOR_AL_DIA_ACTUAL);
         validarFechaNoPuedeSerIgualAlDiaDeDescanso(fechaIngreso, LA_FECHA_NO_PUEDE_SER_IGUAL_AL_DIA_DE_DESCANSO);
@@ -45,12 +43,6 @@ public class MatriculaEstudiante {
         this.idDocenteSala = idDocenteSala;
         this.jornada = jornada;
         this.fechaIngreso = fechaIngreso;
-    }
-
-    public static void validarIdentificadorSalaCorrecto(Long idDocenteSala, String mensaje) {
-        if (idDocenteSala < 1 || idDocenteSala > 5) {
-            throw new ExcepcionValorInvalido(mensaje);
-        }
     }
 
     public static void validarJornadaCorrecto(String jornada, String mensaje) {
