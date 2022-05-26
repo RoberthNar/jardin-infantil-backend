d0fc243714ac513f71a501fbe851649b23eeb045
Roberth Narvaez <roberth.narvaez@ceiba.com.co>
2022-05-26 09:04:37 -0500

    Adicionando validaciones de pension

--- microservicio/dominio/src/main/java/com/ceiba/matricula_estudiante/puerto/dao/DaoMatriculaEstudiante.java
+++ microservicio/dominio/src/main/java/com/ceiba/matricula_estudiante/puerto/dao/DaoMatriculaEstudiante.java
@@ -2,7 +2,6 @@ package com.ceiba.matricula_estudiante.puerto.dao;
 
 import com.ceiba.matricula_estudiante.modelo.dto.DtoMatriculaEstudiante;
 
-import java.util.List;
 
 public interface DaoMatriculaEstudiante {
 
